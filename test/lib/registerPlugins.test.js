3ec38c56787785e6cc110a7164de7e6496ca6b5e
Dao Lam <daolam112@gmail.com>
2017-03-06 12:04:32 -0800

    register templates

--- test/lib/registerPlugins.test.js
+++ test/lib/registerPlugins.test.js
@@ -22,6 +22,7 @@ describe('Register Unit Test Case', () => {
         '../plugins/jobs',
         '../plugins/pipelines',
         '../plugins/secrets',
+        '../plugins/templates',
         '../plugins/webhooks',
         '../plugins/stats'
     ];
